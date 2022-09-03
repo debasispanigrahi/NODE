a2c22b69e099786c92fd616c6f146d78f60f68ca
Debasish <debasish@weassemble.team>
2022-09-04 01:20:09 +0530

    phase 2

--- Helpers/constants.helper.js
+++ Helpers/constants.helper.js
@@ -1,3 +1,3 @@
 exports.userStatusList=["hidden","pending","active","online","disabled"]
-exports.planList=["gold","silver","plan"]
+exports.planList=["gold","silver","basic"]
 exports.fieldListAdmin=["full_name","email","number","is_admin","plan","status","active_till","updatedAt","createdAt"]
